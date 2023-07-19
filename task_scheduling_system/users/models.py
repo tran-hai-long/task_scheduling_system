a1e0186e7725bf547ab86014eb21ca1b6b878252
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-07-19 23:00:15 +0700

    Create CustomUser model

---
+++ task_scheduling_system/users/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db.models import EmailField, CharField
+from .managers import MyUserManager
+
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = EmailField("Email address", unique=True)
+    full_name = CharField("Name of User", blank=True, max_length=255)
+    username = CharField("Username", max_length=64, unique=True)
+    first_name = None
+    last_name = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name", "username"]
+
+    objects = MyUserManager()
+
+    def __str__(self):
+        return self.email
