d45a12adc441e2f4e819c14c7ad838f341b0b9e1
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-07-24 02:09:49 +0700

    Create API view to retrieve, update, destroy a particular task

--- task_scheduling_system/tasks/api/views.py
+++ task_scheduling_system/tasks/api/views.py
@@ -1,8 +1,13 @@
-from rest_framework import generics
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from .serializers import TaskSerializer
 from ..models import Task
 
 
-class TaskListAPIView(generics.ListAPIView):
+class TaskListAPIView(ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
--- task_scheduling_system/tasks/urls.py
+++ task_scheduling_system/tasks/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-from task_scheduling_system.tasks.api.views import TaskListAPIView
+from task_scheduling_system.tasks.api.views import TaskListAPIView, \
+    TaskRetrieveUpdateDestroyAPIView
 
-urlpatterns = [path(route='api/v1/', view=TaskListAPIView.as_view(), name='task_list_api')]
+urlpatterns = [
+    path(route='api/v1/', view=TaskListAPIView.as_view(), name='task_list_api'),
+    path(route='api/v1/<int:pk>', view=TaskRetrieveUpdateDestroyAPIView.as_view(), name='task_retrieve_update_destroy_api'),
+    ]
