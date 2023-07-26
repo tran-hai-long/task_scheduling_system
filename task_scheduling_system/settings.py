4817dfac1da3ee528fef09004462a4a59bc796c1
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-07-27 04:41:45 +0700

    Support pagination for getting list of tasks

--- task_scheduling_system/settings.py
+++ task_scheduling_system/settings.py
@@ -131,6 +131,8 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
 }
 
 REST_AUTH = {
