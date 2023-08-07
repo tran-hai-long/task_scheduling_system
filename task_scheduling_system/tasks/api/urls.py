987e3161fc706c249fb4dec55953ef82848da646
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-08-07 16:31:34 +0700

    Neatly format urls.py

--- task_scheduling_system/tasks/api/urls.py
+++ task_scheduling_system/tasks/api/urls.py
@@ -6,7 +6,7 @@ from ..api.views import TaskListCreateAPIView, TaskRetrieveUpdateDestroyAPIView,
 urlpatterns = [
     path(route='tasks/', view=TaskListCreateAPIView.as_view(),
          name='task_list_create_api'),
-    path(route='task/<int:pk>', view=TaskRetrieveUpdateDestroyAPIView.as_view(),
+    path(route='task/<int:pk>/', view=TaskRetrieveUpdateDestroyAPIView.as_view(),
          name='task_retrieve_update_destroy_api'),
     path(route='to-do/', view=TodoListAPIView.as_view(), name='todo_list_api'),
 ]
--- task_scheduling_system/users/api/urls.py
+++ task_scheduling_system/users/api/urls.py
@@ -5,6 +5,6 @@ from ..api.views import CustomUserListAPIView, CustomUserRetrieveUpdateDestroyAP
 urlpatterns = [
     path(route='users/', view=CustomUserListAPIView.as_view(),
          name='custom_user_list_api'),
-    path(route='user/<int:pk>', view=CustomUserRetrieveUpdateDestroyAPIView.as_view(),
+    path(route='user/<int:pk>/', view=CustomUserRetrieveUpdateDestroyAPIView.as_view(),
          name='custom_user_retrieve_update_destroy_api'),
 ]
