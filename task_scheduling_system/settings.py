a46ae0d6ab365d3222ced85b4cab457ded3afc4c
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-08-18 13:01:41 +0700

    Disable pagination

--- task_scheduling_system/settings.py
+++ task_scheduling_system/settings.py
@@ -147,8 +147,6 @@ REST_FRAMEWORK = {
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
 }
 
 REST_AUTH = {
